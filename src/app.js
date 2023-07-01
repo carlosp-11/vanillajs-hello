8b60df2cb1bd372dbb89ca519093233d20e2e83a
carlosp-11 <135666468+carlosp-11@users.noreply.github.com>
2023-07-01 18:22:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#bton").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = makeExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let makeExcuse = () => {
+  function obtRandNum(num) {
+    let randomNumber = Math.floor(Math.random() * num);
+    return randomNumber;
+  }
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomExcuse = who[obtRandNum(who.length)].concat(
+    " ",
+    action[obtRandNum(action.length)],
+    " ",
+    what[obtRandNum(what.length)],
+    " ",
+    when[obtRandNum(when.length)]
+  );
+
+  return randomExcuse;
 };
